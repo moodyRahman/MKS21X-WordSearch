e69279df2338bbdedd2bc013bedc8179a37fd649
Moody <mrahman01@stuy.edu>
2018-11-09 21:51:28 -0500

    added more private fields, reinstated horizontal add word, added addWord generic

--- WordSearch.java
+++ WordSearch.java
@@ -1,7 +1,16 @@
+import java.util.*;
+import java.io.*;
+
 public class WordSearch{
     private char[][]data;
+    private int seed;
+    private Random randgen;
 	private int rowLength;
 	private int coLength;
+    //all words from a text file get added to wordsToAdd, indicating that they have not yet been added
+    private ArrayList<String>wordsToAdd;
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
 
     /**Initialize the grid to the size specified
 
@@ -71,20 +80,20 @@ public class WordSearch{
      * and the board is NOT modified.
 
      */
-    public boolean addWordHorizontal(String word,int row, int col){
-		if (col + word.length() > coLength || col < 0){
-			return false;
-		}
-		if (row >= rowLength - 1 || row < 0){
-			return false;
-		}
-		int temp = 0;
-		for (int x = col; x < word.length() + col; x++){
-			data[x][row] = word.charAt(temp);
-			temp++;
-		}
-		return true;
-    }
+     public boolean addWordHorizontal(String word,int row, int col){
+ 		if (col + word.length() > coLength || col < 0){
+ 			return false;
+ 		}
+ 		if (row >= rowLength - 1 || row < 0){
+ 			return false;
+ 		}
+ 		int temp = 0;
+ 		for (int x = col; x < word.length() + col; x++){
+ 			data[x][row] = word.charAt(temp);
+ 			temp++;
+ 		}
+        return true;
+     }
 
 
    /**Attempts to add a given word to the specified position of the WordGrid.
@@ -144,7 +153,26 @@ public class WordSearch{
         return true;
     }
 
-    public boolean addWord(String word, int row, int col, int rowIncrement, int colIncrement){
-        return true;
-    }
+    /**Attempts to add a given word to the specified position of the WordGrid.
+    *The word is added in the direction rowIncrement,colIncrement
+    *Words must have a corresponding letter to match any letters that it overlaps.
+    *
+    *@param word is any text to be added to the word grid.
+    *@param row is the vertical locaiton of where you want the word to start.
+    *@param col is the horizontal location of where you want the word to start.
+    *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+    *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+    *@return true when: the word is added successfully.
+    *        false when: the word doesn't fit, OR  rowchange and colchange are both 0,
+    *        OR there are overlapping letters that do not match
+    */
+   public boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
+       return true;
+   }
+
+   /*[rowIncrement,colIncrement] examples:
+    *[-1,1] would add up and the right because (row -1 each time, col + 1 each time)
+    *[ 1,0] would add downwards because (row+1), with no col change
+    *[ 0,-1] would add towards the left because (col - 1), with no row change
+    */
 }
