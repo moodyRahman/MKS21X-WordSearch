a0881115d024f53440ea06ed80207da07050f264
Moody <mrahman01@stuy.edu>
2018-11-11 13:21:54 -0500

    more driver tests

--- Driver.java
+++ Driver.java
@@ -1,21 +1,45 @@
+import java.util.*;
+
 public class Driver{
 	public static void main(String[] args) {
-		WordSearch a = new WordSearch(10,10, "");
-		WordSearch b = new WordSearch(10, 10, "");
-		a.editor(0, 0, 'm');
-		a.editor(9, 9, 'a');
-
-		a.editor(0, 9, 'd');
+		WordSearch a = new WordSearch(10,10, "words.txt");
+		a.addAll();
 		System.out.println(a);
 
-		System.out.println(a.addWordHorizontalBackwards("jpd", 0, 7));
-		System.out.println(a.addWordHorizontalBackwards("cat", 1, 0));
-		System.out.println(a);
-		System.out.println();
-		System.out.println();
+		// Random r = new Random();
+		// for (int x = 0; x < 100; x++){
+		// 	System.out.println(r.nextInt() % 2 );
+		// }
+		// WordSearch b = new WordSearch(10, 10, "words.txt");
+		// a.editor(0, 0, 'm');
+		// a.editor(9, 9, 'a');
+		//
+		// a.editor(0, 9, 'd');
+		// System.out.println(a);
+		//
+		// System.out.println(a.addWordHorizontalBackwards("jpd", 0, 7));
+		// System.out.println(a.addWordHorizontalBackwards("cat", 1, 0));
+		// System.out.println(a);
+		// System.out.println();
+		// System.out.println();
+		//
+		// System.out.println();
+		// System.out.println();
+		//
+		// System.out.println(b);
+		//
+		// System.out.println();
+		// System.out.println();
+		// System.out.println();
+		//
+		// WordSearch d = new WordSearch(10, 10, "words.txt");
+		// System.out.println(d);
+		// d.addWord("osbfucate", 8, 0, 0, 1);
+		// d.addWord("catos", 4, 5, 1, -1);
+		// System.out.println(d);
 
-		System.out.println();
-		System.out.println();
+
+
 		// System.out.println(b.addWord("obs", 5, 5, 1, 1));
 		// System.out.println(b.wordchecker("caa", 5, 4, 1, 1));
 		// System.out.println(b.wordchecker("caa", 5, 4, 1, -1));
@@ -26,17 +50,6 @@ public class Driver{
 		// System.out.println(b.wordchecker("caa", 5, 4, 0, 1));
 		// System.out.println(b.wordchecker("caa", 5, 4, -1, 0));
 		// System.out.println(b.wordchecker("caa", 5, 4, 0, -1));
-		System.out.println(b);
-
-		System.out.println();
-		System.out.println();
-		System.out.println();
-
-		WordSearch d = new WordSearch(10, 10, "");
-		System.out.println(d);
-		d.addWord("osbfucate", 8, 0, 0, 1);
-		d.addWord("catos", 4, 5, 1, -1);
-		System.out.println(d);
 		/**
 		System.out.println("");
 		System.out.println(a.addWordVertical("neko", 6, 0));
